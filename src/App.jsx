71081530170897f429c21aa62a08e1ab96048d4b
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-24 02:11:51 +0200

    +display NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+// import { Routes, Route } from 'react-router-dom';
+
+import NavBar from './components/NavBar/NavBar';
+
 function App() {
-  return;
+  return <NavBar />;
 }
 
 export default App;
