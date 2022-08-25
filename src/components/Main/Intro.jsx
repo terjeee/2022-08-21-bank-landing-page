ef6d73e02b1bf0223188d4630c4159a526061c8f
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-25 03:39:32 +0200

    :)

---
+++ src/components/Main/Intro.jsx
@@ -0,0 +1,24 @@
+import ButtonInvite from '../UI/ButtonInvite';
+
+import Mockups from '../../assets/images/image-mockups.png';
+import css from './Intro.module.css';
+
+function Intro() {
+  return (
+    <section className={css.intro}>
+      <div className={css.div01}>
+        <img src={Mockups} alt='mobile phones' />
+      </div>
+      <div className={css.div02}>
+        <h1>Next Generation digital banking</h1>
+        <p>
+          Take your financial life online. Your Easybank account will be a one-stop-shop for
+          spending, sacing, budgeting, investing and much more.
+        </p>
+        <ButtonInvite />
+      </div>
+    </section>
+  );
+}
+
+export default Intro;
