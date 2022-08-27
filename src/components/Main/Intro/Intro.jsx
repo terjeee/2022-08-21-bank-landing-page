95399e506e530dd25f5a76f00e171a125e322201
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-27 22:52:19 +0200

    test intro

--- src/components/Main/Intro/Intro.jsx
+++ src/components/Main/Intro/Intro.jsx
@@ -1,21 +1,23 @@
-import ButtonInvite from '../../UI/ButtonInvite';
+import ButtonInvite from "../../UI/ButtonInvite";
 
-import Mockups from '../../../assets/images/image-mockups.png';
-import css from './Intro.module.css';
+import Mockups from "../../../assets/images/image-mockups.png";
+import css from "./Intro.module.css";
 
 function Intro() {
   return (
     <section className={css.intro}>
-      <div className={css.div01}>
-        <img src={Mockups} alt='mobile phones' />
-      </div>
-      <div className={css.div02}>
-        <h1>Next generation digital banking</h1>
-        <p>
-          Take your financial life online. Your Easybank account will be a one-stop-shop for
-          spending, saving, budgeting, investing and much more.
-        </p>
-        <ButtonInvite />
+      <div className="max-width__container">
+        <div className={css.div01}>
+          <img src={Mockups} alt="mobile phones" />
+        </div>
+        <div className={css.div02}>
+          <h1>Next generation digital banking</h1>
+          <p>
+            Take your financial life online. Your Easybank account will be a one-stop-shop for
+            spending, saving, budgeting, investing and much more.
+          </p>
+          <ButtonInvite />
+        </div>
       </div>
     </section>
   );
