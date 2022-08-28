a0f802f2bb7c87f662cffda03989cc0eabe5b3de
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-28 04:29:12 +0200

    useNavigate

--- src/components/Main/Articles/components/Article.jsx
+++ src/components/Main/Articles/components/Article.jsx
@@ -1,14 +1,25 @@
-import css from './Article.module.css';
+import { useNavigate, useParams } from "react-router-dom";
+
+import css from "./Article.module.css";
 
 function Article(props) {
+  const navigate = useNavigate();
+  // const {articleId} = useParams()
+
+  const id = props.id;
   const author = props.author;
   const title = props.title;
   const text = props.text;
   const image = props.image;
   const image_alt = props.image_alt;
 
+  const handleNavigate = () => {
+    // NAVIGATE TO ARTICLE-ID?
+    navigate(`/articles`);
+  };
+
   return (
-    <article className={css.article}>
+    <article className={css.article} onClick={handleNavigate}>
       <img src={image} alt={image_alt} />
       <div className={css.article__child}>
         <h5 className={css.author}>by {author}</h5>
