38233533953eae147d7a1490acca8cc44298c8ef
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-28 04:31:01 +0200

    max-width for readability

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,28 @@
 // import { Routes, Route } from 'react-router-dom';
 
-import MenuMobile from './components/MenuMobile';
-import MenuDesktop from './components/MenuDesktop';
-import ButtonInvite from '../UI/ButtonInvite';
+import MenuMobile from "./components/MenuMobile";
+import MenuDesktop from "./components/MenuDesktop";
+import ButtonInvite from "../UI/ButtonInvite";
 
-import BankLogo from '../../assets/images/logo.svg';
-import css from './NavBar.module.css';
+import BankLogo from "../../assets/images/logo.svg";
+import css from "./NavBar.module.css";
 
 function NavBar() {
   return (
     <nav className={css.NavBar}>
-      <div>
-        <img src={BankLogo} alt='logo' />
-      </div>
-      <div>
-        <MenuMobile />
-        <MenuDesktop />
-      </div>
-      <div className={css.ButtonInvite}>
-        <ButtonInvite />
+      <div className="max-width__container">
+        <div className={css.NavBar__content}>
+          <div>
+            <img src={BankLogo} alt="logo" />
+          </div>
+          <div>
+            <MenuMobile />
+            <MenuDesktop />
+          </div>
+          <div className={css.ButtonInvite}>
+            <ButtonInvite />
+          </div>
+        </div>
       </div>
     </nav>
   );
