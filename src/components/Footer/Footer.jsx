7baf95a803125455203b1b4c5ed19a8a6a16a4f0
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-31 23:51:04 +0200

    idk

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,33 +15,39 @@ function Footer() {
   return (
     <footer className={css.footer}>
       <div className="max-width__container">
-        <div className={css.div01}>
-          <SvgBankLogo className={css.logo} />
-          <div>
-            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
-              <img src={svgFacebook} alt="icon-facebook" />
-            </a>
-            <a href="https://www.youtube.com" target="_blank" rel="noreferrer">
-              <img src={svgYoutube} alt="icon-youtube" />
-            </a>
-            <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
-              <img src={svgTwitter} alt="icon-twitter" />
-            </a>
-            <a href="https://www.pinterest.com" target="_blank" rel="noreferrer">
-              <img src={svgPinterest} alt="icon-pinterest" />
-            </a>
-            <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
-              <img src={svgInstagram} alt="icon-instagram" />
-            </a>
+        <div className={css.wrapper}>
+          <div className={css.div01}>
+            <SvgBankLogo className={css.logo} />
+            <div>
+              <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+                <img src={svgFacebook} alt="icon-facebook" />
+              </a>
+              <a href="https://www.youtube.com" target="_blank" rel="noreferrer">
+                <img src={svgYoutube} alt="icon-youtube" />
+              </a>
+              <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
+                <img src={svgTwitter} alt="icon-twitter" />
+              </a>
+              <a href="https://www.pinterest.com" target="_blank" rel="noreferrer">
+                <img src={svgPinterest} alt="icon-pinterest" />
+              </a>
+              <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+                <img src={svgInstagram} alt="icon-instagram" />
+              </a>
+            </div>
           </div>
-        </div>
-        <div className={css.div02}>
-          <Link to="/about-us">About Us</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/blog">Blog</Link>
-          <Link to="/careers">Careers</Link>
-          <Link to="/support">Support</Link>
-          <Link to="/privacy-policy">Privacy Policy</Link>
+          <div className={css.div02}>
+            <div>
+              <Link to="/about-us">About Us</Link>
+              <Link to="/contact">Contact</Link>
+              <Link to="/blog">Blog</Link>
+            </div>
+            <div>
+              <Link to="/careers">Careers</Link>
+              <Link to="/support">Support</Link>
+              <Link to="/privacy-policy">Privacy Policy</Link>
+            </div>
+          </div>
         </div>
         <div className={css.div03}>
           <ButtonInvite />
