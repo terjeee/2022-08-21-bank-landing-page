77a65ffe72faee829cbb1a84ab59b7eb94abfbef
terjeee <96795863+terjeee@users.noreply.github.com>
2022-09-01 05:36:29 +0200

    :

--- src/components/NavBar/components/MenuDesktop.jsx
+++ src/components/NavBar/components/MenuDesktop.jsx
@@ -1,23 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import css from './MenuDesktop.module.css';
+import css from "./MenuDesktop.module.css";
 
 function MenuDesktop() {
   return (
     <nav className={css.menu}>
-      <Link className={css.menu__item} to='/'>
+      <Link className={css.menu__item} to="/">
         Home
       </Link>
-      <Link className={css.menu__item} to='/about'>
+      <Link className={css.menu__item} to="/about">
         About
       </Link>
-      <Link className={css.menu__item} to='/contact'>
+      <Link className={css.menu__item} to="/contact">
         Contact
       </Link>
-      <Link className={css.menu__item} to='/blog'>
+      <Link className={css.menu__item} to="/blog">
         Blog
       </Link>
-      <Link className={css.menu__item} to='/careers'>
+      <Link className={css.menu__item} to="/careers">
         Careers
       </Link>
     </nav>
