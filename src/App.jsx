d0279414787796c6267fa7dccf6908f1f7230b75
terjeee <96795863+terjeee@users.noreply.github.com>
2022-09-03 06:11:28 +0200

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-// import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar/NavBar";
-import Intro from "./components/Main/Intro/Intro";
-import WhyUs from "./components/Main/WhyUs/WhyUs";
-import Articles from "./components/Main/Articles/Articles";
 import Footer from "./components/Footer/Footer";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
+import Careers from "./pages/Careers";
+import Support from "./pages/Support";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Invitation from "./pages/Invitation";
 
 function App() {
   return (
     <>
       <NavBar />
       <main>
-        <Intro />
-        <WhyUs />
-        <Articles />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/invitation" element={<Invitation />} />
+        </Routes>
       </main>
       <Footer />
     </>
